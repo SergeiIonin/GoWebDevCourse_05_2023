02e1591d7a1480cba10a1db2a9f67ad0f3f29cb6
SergeiIonin <sai1042@yandex.ru>
2023-04-20 19:09:59 -0400

    add parsing to templates

---
+++ templates/parseexecute/performantparsing/performantParsingApp.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	err := tpl.Execute(os.Stdout, nil) // will return only the 1st template
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "three.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
