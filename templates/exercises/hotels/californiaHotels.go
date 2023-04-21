49da38d4412ad7235485108091d4287614c42d96
SergeiIonin <sai1042@yandex.ru>
2023-04-21 15:11:38 -0400

    refactor exercises/hotels

--- templates/exercises/hotels/californiaHotels.go
+++ templates/exercises/hotels/californiaHotels.go
@@ -1,24 +1,58 @@
 package main
 
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
 type region string
 
+// todo how can we enforce using only these constants for `Region`?
 const Southern region = "Southern"
 const Central region = "Central"
 const Northern region = "Northern"
 
+// type hotels []hotel is also possible
 type hotels struct {
-	List   []hotel
-	Region region
+	List []hotel
 }
 
 type hotel struct {
-	Name    string
-	Address string
-	City    string
-	Zip     int
-	Region  string
+	Name, Address, City string
+	Region              region
+	Zip                 int
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
 func main() {
+	hotelsList := hotels{
+		[]hotel{
+			hotel{
+				Name:    "Hotel California",
+				Address: "42 Sunset Boulevard",
+				City:    "Los Angeles",
+				Zip:     95612,
+				Region:  Southern,
+			},
+			hotel{
+				Name:    "H",
+				Address: "4",
+				City:    "L",
+				Zip:     95612,
+				Region:  Southern,
+			},
+		},
+	}
+
+	err := tpl.Execute(os.Stdout, hotelsList)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 }
