2e599b153b2c00b40eb2826999a73c382a352f45
SergeiIonin <sai1042@yandex.ru>
2023-04-24 22:38:05 -0400

    add serve goroutine in routing/exercises/02.../basicTCPserverWIthReadApp.go

--- routing/exercises/02/2_basictcpserverwithread/basicTCPserverWIthReadApp.go
+++ routing/exercises/02/2_basictcpserverwithread/basicTCPserverWIthReadApp.go
@@ -3,10 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 )
+
+func serve(conn net.Conn) {
+	defer conn.Close()
+	sc := bufio.NewScanner(conn)
+	for sc.Scan() {
+		text := sc.Text()
+		fmt.Println("text = ", text)
+		if text == "" {
+			fmt.Println("breaking...")
+			break
+		}
+	}
+}
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
@@ -21,23 +33,6 @@ func main() {
 			log.Fatalln(err)
 			continue
 		}
-
-		sc := bufio.NewScanner(conn)
-		var resp string
-		for sc.Scan() {
-			text := sc.Text()
-			fmt.Println("text = ", text)
-			if text == "" {
-				fmt.Println("breaking...")
-				break
-			}
-			resp = resp + "\n" + text
-		}
-		fmt.Println("out of the loop")
-
-		fmt.Println("Code got here.") // after some time the goroutine closes conn and we'll see this line, but not the next!
-		io.WriteString(conn, "I see you connected.")
-
-		conn.Close()
+		go serve(conn)
 	}
 }
