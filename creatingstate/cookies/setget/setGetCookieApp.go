ba0ea5b1b3c688c35f6420d15e1823229f016549
SergeiIonin <sai1042@yandex.ru>
2023-04-27 14:06:11 -0300

    add creatingstate/cookies/setget

---
+++ creatingstate/cookies/setget/setGetCookieApp.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", set)
+	http.HandleFunc("/read", read)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func set(w http.ResponseWriter, req *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "my-cookie",
+		Value: "123",
+		Path:  "/",
+	})
+
+	fmt.Fprintln(w, "COOKIE WRITTEN - CHECK YOUR BROWSER")
+}
+
+func read(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("my-cookie")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintln(w, "YOUR COOKIE:", c)
+}
